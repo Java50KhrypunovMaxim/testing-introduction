c89eeb3d789ece2608270d3a308194f33141b9e6
Maxim Khrypunov <khrypunovmax@gmail>
2023-05-23 20:57:58 +0300

    CW #1 Unit testing introduction

---
+++ src/telran/numbers/Calculator.java
@@ -0,0 +1,31 @@
+package telran.numbers;
+
+public class Calculator 
+{
+public static int sum (int op1, int op2)
+{
+	int res = op1+op2;
+	return res;
+}
+public static int subtract (int op1, int op2)
+{
+	int res = op1-op2;
+	return res;
+}
+public static int multiply (int op1, int op2)
+{
+	int res = op1*op2;
+	return res;
+}
+public static int divide (int op1, int op2)
+{
+	int res = op1/op2;
+	return res;
+}
+public static int remainder (int op1, int op2)
+{
+	int res = op1%op2;
+	return res;
+}
+
+}
